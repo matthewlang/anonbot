bb16bd6a6cfd6cef8d360912268c6d87625c4fde
Matt Lang <langma@gmail.com>
2021-08-18 11:59:20 -0700

    bump boot disk size

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,7 @@
 gcloud compute instances create-with-container ${AB_VM} \
   --container-image gcr.io/${AB_GCP_PROJECT}/anonbot:${AB_VERSION_TAG} \
   --machine-type f1-micro \
+  --boot-disk-size=1TB \
   --tags app \
   --address ${AB_GCP_IP} \
   --container-arg="-oauth=${AB_OAUTH}" \
